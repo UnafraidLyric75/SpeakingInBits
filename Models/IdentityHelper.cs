6fefb5f30825874855c20e3a6f799fb03ee67371
John Uthus <john.a.uthus.jr1@gmail.com>
2020-09-28 16:27:55 -0700

    added options

---
+++ Models/IdentityHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpeakingInBits.Models
+{
+    public static class IdentityHelper
+    {
+        public static void SetIdentityOptions(IdentityOptions options)
+        {
+            // set sign in options
+            options.SignIn.RequireConfirmedEmail = false;
+            options.SignIn.RequireConfirmedPhoneNumber = false;
+
+            // set password strength
+            options.Password.RequireDigit = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequiredLength = 8;
+            options.Password.RequireNonAlphanumeric = false;
+        }
+    }
+}
