64d7dc21576bf1fc4445868d37311729d92dbf4f
John Uthus <john.a.uthus.jr1@gmail.com>
2020-10-13 20:26:09 -0700

    Create VideoLesson model

---
+++ Models/Lesson.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpeakingInBits.Models
+{
+    /// <summary>
+    /// A generic course lesson
+    /// </summary>
+    public abstract class Lesson
+    {
+        [Key]
+        public int LessonId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+
+    /// <summary>
+    /// Video lecture lesson
+    /// </summary>
+    public class VideoLesson : Lesson
+    {
+        /// <summary>
+        /// Embed code for video. ex Youtube/Vimeo
+        /// </summary>
+        public string EmbedCode { get; set; }
+    }
+}
